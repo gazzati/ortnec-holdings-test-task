2ec4ddfcf59e9e111609da66624e5f88750d5303
gazzati <gazzaevtimur@gmail.com>
2020-08-05 19:53:29 +0300

    Updates

---
+++ precache-manifest.56e13e613d135c9fcc4b0bcb87ddff79.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d965a3f5c1c97e7381ed4833cd3c0045",
+    "url": "/OrtnecHoldings-test-task/index.html"
+  },
+  {
+    "revision": "7ea0bca1b08e4048e7fe",
+    "url": "/OrtnecHoldings-test-task/static/css/2.40c68a69.chunk.css"
+  },
+  {
+    "revision": "07983e709670ee0f0f24",
+    "url": "/OrtnecHoldings-test-task/static/css/main.619e5837.chunk.css"
+  },
+  {
+    "revision": "7ea0bca1b08e4048e7fe",
+    "url": "/OrtnecHoldings-test-task/static/js/2.19c946bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/OrtnecHoldings-test-task/static/js/2.19c946bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07983e709670ee0f0f24",
+    "url": "/OrtnecHoldings-test-task/static/js/main.c7bbbe05.chunk.js"
+  },
+  {
+    "revision": "f3dc6478679b6eb6ccb9",
+    "url": "/OrtnecHoldings-test-task/static/js/runtime-main.923f1ee4.js"
+  },
+  {
+    "revision": "9166a6ecf3a871029ab2570b7e77918b",
+    "url": "/OrtnecHoldings-test-task/static/media/1.9166a6ec.jpg"
+  },
+  {
+    "revision": "f27cb0aa0134a926ac6edd5d8c82b47f",
+    "url": "/OrtnecHoldings-test-task/static/media/1.f27cb0aa.svg"
+  },
+  {
+    "revision": "2061c8b217415624b1a3e7761be9cc87",
+    "url": "/OrtnecHoldings-test-task/static/media/2.2061c8b2.svg"
+  },
+  {
+    "revision": "76b1f8ebcc4f7cf032d8f4b3a35cd3b4",
+    "url": "/OrtnecHoldings-test-task/static/media/2.76b1f8eb.jpg"
+  },
+  {
+    "revision": "40462dbe8e645ad9a238fd1fe010953e",
+    "url": "/OrtnecHoldings-test-task/static/media/3.40462dbe.jpg"
+  },
+  {
+    "revision": "ba782519b0b4b3e7ed1f0b40e4c9ee13",
+    "url": "/OrtnecHoldings-test-task/static/media/3.ba782519.svg"
+  },
+  {
+    "revision": "2ca2175ff53f21791effce89f85d103c",
+    "url": "/OrtnecHoldings-test-task/static/media/4.2ca2175f.jpg"
+  },
+  {
+    "revision": "81a997d4a416a99dd5b0380bcce675b4",
+    "url": "/OrtnecHoldings-test-task/static/media/close.81a997d4.svg"
+  }
+]);
